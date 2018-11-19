b5fbb0a4ea175fdcf93444b3040d2e559031c0e9
rebeccarollins <rrollins97@claytonschools.net>
2018-11-19 14:55:03 -0600

    lkjl;k

--- team11.py
+++ team11.py
@@ -18,9 +18,9 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'C' or 'b'. 
     '''
     if their_history=="":
-        return 'B'
+        return 'b'
     else:
-        return 'B'
+        return 'b'
     
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -30,7 +30,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
