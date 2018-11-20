8b36600ff320853e1c26f2eab69b5869c234fa5c
AsianSensationnn <44909402+AsianSensationnn@users.noreply.github.com>
2018-11-20 14:42:07 -0600

    hi

--- team6.py
+++ team6.py
@@ -11,12 +11,12 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+    if their_history== '':
+       return 'b'
+    elif their_history[-1]=='c':
+       return 'c'
+    else:
+        return 'b'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
